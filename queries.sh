be65b0bb2d74d06889995bb5b17b134da8ff4ab4
dinahess <dina.hess@gmail.com>
2025-04-03 11:31:29 -0500

    Add files via upload

--- queries.sh
+++ queries.sh
@@ -11,13 +11,13 @@ echo -e "\nTotal number of goals in all games from both teams combined:"
 echo "$($PSQL "SELECT SUM(winner_goals) + SUM(opponent_goals) FROM games")"
 
 echo -e "\nAverage number of goals in all games from the winning teams:"
-echo "$($PSQL "SELECT AVG(winner_goals) FROM games")"
+echo "$($PSQL "SELECT ROUND(AVG(winner_goals),16) FROM games")"
 
 echo -e "\nAverage number of goals in all games from the winning teams rounded to two decimal places:"
 echo "$($PSQL "SELECT ROUND(AVG(winner_goals),2) FROM games")"
 
 echo -e "\nAverage number of goals in all games from both teams:"
-echo "$($PSQL "SELECT AVG(winner_goals + opponent_goals) FROM games")"
+echo "$($PSQL "SELECT ROUND(AVG(winner_goals + opponent_goals),16) FROM games")"
 
 echo -e "\nMost goals scored in a single game by one team:"
 echo "$($PSQL "SELECT MAX(GREATEST(winner_goals, opponent_goals)) FROM games")"
@@ -26,35 +26,16 @@ echo -e "\nNumber of games where the winning team scored more than two goals:"
 echo "$($PSQL "SELECT COUNT(*) FROM games WHERE winner_goals > 2")"
 
 echo -e "\nWinner of the 2018 tournament team name:"
-echo "$($PSQL "select t.name from teams as t 
-                inner join games as g 
-                on t.team_id = g.winner_id 
-                and g.year = 2018
-                and g.round = 'Final'")"
+echo "$($PSQL "select t.name from teams as t inner join games as g on t.team_id = g.winner_id and g.year = 2018 and g.round = 'Final'")"
 
 echo -e "\nList of teams who played in the 2014 'Eighth-Final' round:"
-echo "$($PSQL "select t.name from teams as t 
-                inner join games as g 
-                on (t.team_id = g.winner_id or t.team_id = g.opponent_id)
-                and g.year = 2014
-                and g.round = 'Eighth-Final'
-                group by t.name")"
+echo "$($PSQL "select t.name from teams as t inner join games as g on (t.team_id = g.winner_id or t.team_id = g.opponent_id) and g.year = 2014 and g.round = 'Eighth-Final' group by t.name")"
 
 echo -e "\nList of unique winning team names in the whole data set:"
-echo "$($PSQL "select t.name from teams as t
-                inner join games as g
-                on t.team_id = g.winner_id
-                group by t.name
-                order by t.name")"
+echo "$($PSQL "select t.name from teams as t inner join games as g on t.team_id = g.winner_id group by t.name order by t.name")"
 
 echo -e "\nYear and team name of all the champions:"
-echo "$($PSQL "select g.year, t.name from teams as t
-                inner join games as g
-                on t.team_id = g.winner_id
-                where g.round = 'Final'
-                group by g.year, t.name")"
-
+echo "$($PSQL "select g.year, t.name from teams as t inner join games as g on t.team_id = g.winner_id where g.round = 'Final' group by g.year, t.name")"
 
 echo -e "\nList of teams that start with 'Co':"
 echo "$($PSQL "select t.name from teams as t where t.name like 'Co%'")"
-            
